071792b70e67d8f91aa34f16032b9fb8bddfbcd0
scartoning2020 <35373637+scartoning2020@users.noreply.github.com>
2018-05-22 16:49:03 +0200

    database

---
+++ DNP1/DNP1/sql/dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [password] VARCHAR(50) NOT NULL, 
+    [level] SMALLINT NOT NULL, 
+    [pseudo] VARCHAR(50) NOT NULL, 
+    [firstName] VARCHAR(50) NOT NULL, 
+    [lastName] VARCHAR(50) NOT NULL, 
+    [date_of_birth] DATE NOT NULL, 
+    [sex] CHAR(1) NOT NULL , 
+    
+)
